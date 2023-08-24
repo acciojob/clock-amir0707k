b81fd098da341d5c61958a63202a7eda7a046dd1
amir0707k <34520003+amir0707k@users.noreply.github.com>
2023-08-24 13:32:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+      const pElement = document.getElementById("timer");
+      setInterval(() => {
+        const timer = new Date().toLocaleString();
+        pElement.innerText = `${timer}`;
+      },1000);
